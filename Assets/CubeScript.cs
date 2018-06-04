299aeb161cfeed842912410d4d15d5ed10a86c11
mirekSBCS <33669390+mirekSBCS@users.noreply.github.com>
2018-06-04 15:33:16 -0400

    Initial commit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    [SerializeField]public bool IsSpinning;
+
+    private void Start()
+    {
+        IsSpinning = false;
+    }
+
+    private void Update()
+    {
+        if(IsSpinning == true)
+        {
+            transform.Rotate(new Vector3(0, 60, 0));
+        }
+    }
+    // Use this for initialization
+    public void SpinTime()
+    {
+        IsSpinning = true;
+    }
+
+    public void SpinTimeIsOver()
+    {
+        IsSpinning = false;
+    }
+}
